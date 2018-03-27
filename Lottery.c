af8ec622b7103e2d63a6403f10c13b8d3e0ec682
Aukero <au_zeedzard@hotmail.com>
2018-03-27 15:29:17 +0700

    Change lottery.c 2 places

--- Lottery.c
+++ Lottery.c
@@ -17,7 +17,7 @@ void main()
     {
        if (YourLotteryNo == WinLotteryNo[i])
        {
-          printf("Congratulations You Won %8.2f baht\n", WinAmount[i]);
+          printf("YEAH!!! You Won %8.2f baht\n", WinAmount[i]);
           won = 1;
        }
     }
@@ -26,6 +26,6 @@ void main()
     printf("Enter your Lottery Number (0 to end): ");
     scanf("%d", &YourLotteryNo);
   }
-  printf("GOODBYE");
+  printf("BYEBYE");
   scanf("%d", &i);
 }
